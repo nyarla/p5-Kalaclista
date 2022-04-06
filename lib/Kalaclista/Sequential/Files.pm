f55aa8bae8b1657738c44e80ee4ffc38d0337d6d
nyarla <nyarla@kalaclista.com>
2022-04-06 16:13:45 +0900

    refactor(Kalaclista::Sequential::Files): rewrite code and tests

--- lib/Kalaclista/Sequential/Files.pm
+++ lib/Kalaclista/Sequential/Files.pm
@@ -1,14 +1,34 @@
+package Kalaclista::Sequential::Files;
+
 use strict;
 use warnings;
 
-package Kalaclista::Sequential::Files;
-
+use Carp qw(confess);
 use Path::Tiny::Glob;
 
-use Class::Accessor::Lite (
-  new => 1,
-  ro  => [qw( process result )],
-);
+use Class::Accessor::Lite ( ro => [qw( handle result )], );
+
+sub new {
+  my ( $class, %args ) = @_;
+
+  my $handle = delete $args{'handle'} // sub { };
+  my $result = delete $args{'result'} // sub { };
+
+  if ( ref $handle ne 'CODE' ) {
+    confess q{argument `handle ` is not subroutine.};
+  }
+
+  if ( ref $result ne 'CODE' ) {
+    confess q{argument `result` is not subroutine.};
+  }
+
+  my $self = bless {}, $class;
+
+  $self->{'handle'} = $handle;
+  $self->{'result'} = $result;
+
+  return $self;
+}
 
 sub run {
   my $self  = shift;
@@ -17,10 +37,10 @@ sub run {
   my $files = pathglob( \@globs );
   my @results;
   while ( defined( my $file = $files->next ) ) {
-    push @results, $self->process->( $self, $file );
+    push @results, $self->handle->($file);
   }
 
-  return $self->result->( $self, @results );
+  return $self->result->(@results);
 }
 
 1;
---
+++ t/Kalaclista-Sequential-Files/10_run.t
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test2::V0;
+
+use Kalaclista::Directory;
+use Kalaclista::Sequential::Files;
+
+my $dirs = Kalaclista::Directory->instance;
+
+sub main {
+  my $testdir = $dirs->rootdir->child('t');
+
+  my $runner = Kalaclista::Sequential::Files->new(
+    handle => sub {
+      my $file = shift;
+
+      like( $file->stringify, qr{\.t$} );
+
+      return q{ok};
+    },
+
+    result => sub {
+      for my $result (@_) {
+        is( $result, 'ok' );
+      }
+    },
+  );
+
+  $runner->run( $testdir->stringify, '**', '*.t' );
+
+  done_testing;
+}
+
+main;
