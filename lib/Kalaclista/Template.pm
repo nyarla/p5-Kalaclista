3f2746c84cbe01623daa4415360653960844d301
nyarla <nyarla@kalaclista.com>
2022-05-30 19:56:13 +0900

    feat(Kalaclista::Template): add new module for processing templates

---
+++ lib/Kalaclista/Template.pm
@@ -0,0 +1,39 @@
+package Kalaclista::Template;
+
+use strict;
+use warnings;
+use utf8;
+
+use Exporter 'import';
+
+our @EXPORT = qw(load);
+
+use Path::Tiny;
+
+sub load {
+  my $ns   = shift;
+  my $path = shift;
+
+  local $@;
+  my $template = eval qq{
+  package Kalaclista::Template::_${ns};
+
+  use strict;
+  use warnings;
+
+  use Kalaclista::HyperScript;
+  use Kalaclista::HyperScript::HTMLUtils;
+  use Kalaclista::Template;
+
+  @{[ path($path)->slurp_utf8 ]}
+};
+
+  if ($@) {
+    my $err = $@;
+    return $err;
+  }
+
+  return $template;
+}
+
+1;
