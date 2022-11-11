3f9560fd096caa8b2d50ad512d4b6bacf7cf4b06
nyarla <nyarla@kalaclista.com>
2022-11-11 20:44:33 +0900

    feat: add WebP generator

---
+++ lib/Kalaclista/Generators/WebP.pm
@@ -0,0 +1,81 @@
+package Kalaclista::Generators::WebP;
+
+use strict;
+use warnings;
+use utf8;
+
+use File::Spec;
+use YAML::XS;
+
+use Kalaclista::Parallel::Tasks;
+use Kalaclista::Files;
+
+my $nproc = `nproc --all --ignore 1`;
+chomp($nproc);
+
+sub generate {
+  my $class = shift;
+  my %args  = @_;
+
+  my $images  = delete $args{'images'};
+  my $distdir = delete $args{'distdir'};
+  my $datadir = delete $args{'datadir'};
+  my $sizes   = delete $args{'scales'};
+
+  my @files = grep { $_ =~ m{\.(png|jpeg|jpg|gif)$} } Kalaclista::Files->find( $images->path );
+
+  my @tasks;
+
+  for my $src (@files) {
+    my $path = $src;
+    substr( $path, 0, length( $images->path ) + 1, '' );
+    $path =~ s{\.[^\.]+}{};
+
+    my $meta = "@{[ $distdir->path ]}/${path}.yaml";
+    my $dest = $distdir->child("${path}");
+    $dest->parent->mkpath;
+
+    push @tasks, [ $meta, $src, $dest->path, $sizes ];
+  }
+
+  my $worker = Kalaclista::Parallel::Tasks->new(
+    handle  => sub { resize( $_[0]->@* ) },
+    threads => $nproc,
+  );
+
+  return $worker->run(@tasks);
+}
+
+sub resize {
+  my ( $meta, $src, $dest, $scales ) = @_;
+
+  my $data = {};
+  my $info = `identify '${src}' | cut -d ' ' -f3`;
+  chomp($info);
+
+  $data->{'src'} = {
+    width  => ( split qr{x}, $info )[0],
+    height => ( split qr{x}, $info )[1],
+  };
+
+  for my $scale ( $scales->@* ) {
+    my $size  = $scale->[0];
+    my $width = $scale->[1];
+
+    next if ( $data->{'src'}->{'width'} < $width );
+
+    my $height = `cwebp -resize ${width} 0 -q 100 '${src}' -o '${dest}_${size}.webp' 2>&1 | grep Dimension | cut -d ' ' -f4`;
+    chomp($height);
+
+    $data->{$size} = {
+      width  => $width,
+      height => $height,
+    };
+  }
+
+  YAML::XS::DumpFile( $meta, $data );
+
+  return {};
+}
+
+1;
---
+++ t/Kalaclista-Generators-WebP/10_resize.t
@@ -0,0 +1,49 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Test2::V0;
+
+use YAML::XS;
+
+use Kalaclista::Path;
+use Kalaclista::Generators::WebP;
+
+sub main {
+  my $rootdir = Kalaclista::Path->detect(qr{^t$});
+  my $tempdir = Kalaclista::Path->tempdir;
+
+  my $src  = $rootdir->child('t/fixtures/images/test.png');
+  my $dest = $tempdir->child('images/image');
+
+  $dest->parent->mkpath;
+
+  my $scales = [
+    [ '1x' => 700 ],
+    [ '2x' => 1400 ],
+  ];
+
+  my $meta = $tempdir->child('meta.yaml');
+
+  Kalaclista::Generators::WebP::resize( $meta->path, $src->path, $dest->path, $scales );
+
+  is(
+    YAML::XS::LoadFile( $meta->path ), {
+      src => {
+        width  => 1024,
+        height => 1024,
+      },
+      '1x' => {
+        width  => 700,
+        height => 700,
+      },
+    }
+  );
+
+  ok( -f $tempdir->child('images/image_1x.webp')->path );
+
+  done_testing;
+}
+
+main;
