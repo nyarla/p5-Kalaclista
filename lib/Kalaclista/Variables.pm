f6d7295f19a76a92817c2509fc4ebcb5d1373986
nyarla <nyarla@kalaclista.com>
2022-12-03 20:02:43 +0900

    refactor: change properties read/write

--- lib/Kalaclista/Variables.pm
+++ lib/Kalaclista/Variables.pm
@@ -5,7 +5,27 @@ use warnings;
 
 use Class::Accessor::Lite (
   new => 1,
-  rw  => [qw( title website description section kind data entries href breadcrumb )],
+  ro  => [
+    qw(
+      contains
+      data
+      is_production
+    )
+  ],
+  rw => [
+    qw(
+      breadcrumb
+      description
+      entries
+      href
+      kind
+      label
+      section
+      summary
+      title
+      website
+    )
+  ],
 );
 
 1;
