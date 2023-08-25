a427b3306894fe976787d206fcfb95a8b1c43a53
nyarla <nyarla@kalaclista.com>
2023-08-25 10:06:35 +0900

    refactor(Kalaclista::Path): use new perl feature of class

--- lib/Kalaclista/Path.pm
+++ lib/Kalaclista/Path.pm
@@ -1,130 +1,120 @@
-package Kalaclista::Path;
-
-use strict;
-use warnings;
-use utf8;
-
-use feature qw(state);
+use v5.38;
+use builtin qw(true false);
+use feature qw(class);
+no warnings qw(experimental);
 
 use FindBin ();
 use File::Spec;
 use File::Temp ();
 
-use Class::Accessor::Lite (
-  new => 1,
-  ro  => [qw( path )],
-  rw  => [qw( temporary )],
-);
+class Kalaclista::Path {
+  field $path : param;
+  field $temporary : param = false;
 
-sub detect {
-  my ( $class, $anchors ) = @_;
+  sub detect {
+    my $class  = shift;
+    my $anchor = shift;
+    my @dirs   = File::Spec->splitdir($FindBin::Bin);
 
-  my @dirs = File::Spec->splitdir($FindBin::Bin);
-  while ( defined( my $dir = pop @dirs ) ) {
-    if ( $dir =~ $anchors ) {
-      return $class->new( path => File::Spec->join(@dirs) );
-    }
-  }
-
-  die "failed to find parent directory of anchors";
-}
-
-sub parent {
-  my $self = shift;
-  my @path = File::Spec->splitdir( $self->path );
-  pop @path;
-
-  return ref($self)->new( path => File::Spec->join(@path) );
-}
-
-sub child {
-  my ( $self, $path ) = @_;
-  return ref($self)->new( path => File::Spec->join( $self->path, $path ) );
-}
-
-sub emit {
-  my ( $self, $content ) = @_;
-
-  utf8::encode($content) if ( utf8::is_utf8($content) );
-
-  open( my $fh, '>', $self->path )
-      or die "failed to open file: @{[ $self->path ]}: $!";
-
-  print $fh $content;
-
-  close($fh) or die "failed to close file: @{[ $self->path ]}: $!";
-
-  return 1;
-}
-
-sub get {
-  my $self = shift;
-
-  open( my $fh, '<', $self->path )
-      or die "failed to open file: @{[ $self->path ]}: $!";
-
-  my $content = do { local $/; <$fh> };
-
-  close($fh)
-      or die "failed to close file: @{[ $self->path ]}: $!";
-
-  return $content;
-}
-
-sub remove {
-  my $self = shift;
-
-  return if ( !-d $self->path && -e $self->path && unlink $self->path );
-  return if ( -d $self->path && rmdir $self->path );
-  return;
-}
-
-sub mkpath {
-  my $self = shift;
-  my @path = File::Spec->splitdir( File::Spec->rel2abs( $self->path ) );
-
-  my $path = q{};
-  while ( defined( my $dirname = shift @path ) ) {
-    $path = File::Spec->join( $path, $dirname );
-
-    if ( !-d $path ) {
-      mkdir($path) or warn "failed to mkdir: ${path}: ${!}";
-    }
-  }
-
-  return $self;
-}
-
-sub tempdir {
-  my ( $class, $format ) = @_;
-  $format //= 'kalaclista_XXXXXXXXXX';
-
-  return $class->new( path => File::Temp::tempdir(), temporary => 1, );
-}
-
-sub tempfile {
-  my ( $class, $format ) = @_;
-  $format //= 'kalaclista_XXXXXXXXXX';
-
-  return $class->new( path => ( File::Temp::tempfile() )[1], temporary => 1 );
-}
-
-sub cleanup {
-  my $path = shift;
-
-  do {
-    if ( $path->temporary ) {
-      if ( !-d $path->path && -e $path->path ) {
-        $path->remove;
+    while ( defined( my $dir = pop @dirs ) ) {
+      if ( $dir =~ $anchor ) {
+        return $class->new( path => File::Spec->join(@dirs) );
       }
-      elsif ( -d $path->path && opendir( my $dh, $path->path ) ) {
+    }
+  }
 
+  sub tempdir {
+    my $class = shift;
+    return $class->new( path => File::Temp::tempdir(), temporary => 1, );
+  }
+
+  sub tempfile {
+    my $class = shift;
+    return $class->new( path => ( File::Temp::tempfile() )[1], temporary => 1 );
+  }
+
+  method path {
+    return $path;
+  }
+
+  method temporary {
+    return $temporary;
+  }
+
+  method parent {
+    my @dirs = File::Spec->splitdir($path);
+    pop @dirs;
+
+    return ref($self)->new( path => File::Spec->join(@dirs) );
+  }
+
+  method child {
+    my $child = shift;
+    return ref($self)->new( path => File::Spec->join( $path, $child ) );
+  }
+
+  method emit {
+    my $content = shift;
+    utf8::encode($content) if utf8::is_utf8($content);
+
+    open( my $fh, '>', $path )
+        or die "failed to open file: ${path}: $!";
+
+    print $fh $content;
+
+    close($fh)
+        or die "failed to close file: ${path}: $!";
+
+    return true;
+  }
+
+  method get {
+    open( my $fh, '<', $path )
+        or die "failed to open file: ${path}: $!";
+
+    my $content = do { local $/; <$fh> };
+
+    close($fh)
+        or die "failed to close file: ${path}: $!";
+
+    return $content;
+  }
+
+  method remove {
+    return if !-d $path && -e $path && unlink $path;
+    return if -d $path && rmdir $path;
+    return;
+  }
+
+  method mkpath {
+    my @path = File::Spec->splitdir( File::Spec->rel2abs($path) );
+
+    my $new = q{};
+    while ( defined( my $dir = shift @path ) ) {
+      $new = File::Spec->join( $new, $dir );
+
+      if ( !-d $new ) {
+        mkdir $new
+            or warn "failed to mkdir: ${new}: $!";
+      }
+    }
+  }
+
+  method cleanup {
+    my $target = $self;
+
+    do {
+      if ( !-d $target->path ) {
+        $target->remove;
+
+      }
+      elsif ( -d $target->path && opendir( my $dh, $target->path ) ) {
         for my $fn ( readdir $dh ) {
-          next if ( $fn eq q{.} || $fn eq q{..} );
-          my $item = $path->child($fn);
+          next if $fn eq q{.} || $fn eq q{..};
+
+          my $item = $target->child($fn);
 
           if ( -d $item->path ) {
-            $item->temporary(1);
             $item->cleanup;
           }
           elsif ( !-d $item->path ) {
@@ -132,18 +122,17 @@ sub cleanup {
           }
         }
 
-        closedir($dh) or die "failed to close dir:@{[ $path->path ]}: $!";
-
-        $path->remove;
+        closedir($dh) or die "failed to close dir: @{[ $target->path ]}: $!";
+        return $target->remove;
       }
-    }
-    return;
-  } while ( defined( $path = $path->parent ) );
-}
 
-sub DESTROY {
-  my $self = shift;
-  $self->cleanup if ( $self->temporary );
-}
+      return;
+    } while ( defined( $target = $target->parent ) );
+  }
+
+  method DESTROY {
+    $self->cleanup if $self->temporary;
+  }
+};
 
 1;
